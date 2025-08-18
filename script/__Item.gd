1933a1f5dbe0fa0aab411514a7c5b193510b5412
DiVaSkibo <d.v.skibo@student.khai.edu>
2025-08-18 21:08:23 +0200

    Modif Item scene
    * Item scene and script
    * Item resource script
    * + scene
    + main scene

--- script/__Item.gd
+++ script/__Item.gd
@@ -11,17 +11,26 @@ var is_focused := false
 	#region	Funcs
 func _ready() -> void:
 	if resource.sprite: sprite.texture = resource.sprite
-	if resource.shape: $CollisionShape2D.shape = resource.shape
-	mass = resource.mass
-	if resource.physics_material: physics_material_override = resource.physics_material
+	if resource.radius: $CollisionShape2D.shape.radius = resource.radius
+	gravity_scale = resource.mass
+func _physics_process(_delta: float) -> void:
+	if not freeze and not get_contact_count(): sprite.rotation_degrees += 11
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed('PickUp'):
-		print(' |: ', self, ' is picked up')
+		if is_focused:
+			freeze = false
+			is_focused = false
+			sprite.scale -= Vector2(.11, .11)
+			apply_force(Vector2(0, -400))
 #endregion
 
 	#region	Signals
 func _on_mouse_entered() -> void:
+	if not freeze: return
 	is_focused = true
+	sprite.scale += Vector2(.11, .11)
 func _on_mouse_exited() -> void:
+	if not freeze: return
 	is_focused = false
+	sprite.scale -= Vector2(.11, .11)
 #endregion
--- script/____ItemResource.gd
+++ script/____ItemResource.gd
@@ -2,6 +2,5 @@ extends Resource
 class_name ItemResource
 
 @export var sprite :Texture2D
-@export var shape :Shape2D
-@export_range(.001, 1000.) var mass :float
-@export var physics_material :PhysicsMaterial
+@export_range(-8., 8.) var mass :float = 1.
+@export var radius :float
