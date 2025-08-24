ec564c012cbe27f498a6539fa9c8176b00aafaf5
DiVaSkibo <d.v.skibo@student.khai.edu>
2025-08-24 14:52:47 +0200

    Creat MapScene
    + MapScene
    * Map scene
    * project texture rendering

---
+++ script/MapScene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var tween :Tween
+
+
+func _ready() -> void:
+	tween = create_tween().set_loops().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.chain().tween_property($ocean, 'scale', Vector2(1.04, 1.04), 4)
+	tween.parallel().tween_property($clouds, 'scale', Vector2(1.04, 1.04), 8)
+	tween.parallel().tween_property($moon, 'rotation_degrees', -1.1, 8)
+	tween.chain().tween_property($ocean, 'scale', Vector2.ONE, 4)
+	tween.parallel().tween_property($clouds, 'scale', Vector2.ONE, 8)
+	tween.parallel().tween_property($moon, 'rotation_degrees', 0, 8)
